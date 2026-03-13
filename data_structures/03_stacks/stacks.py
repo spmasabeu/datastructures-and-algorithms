b44100e895b67d0378a42ccc745b6e3ea4899d1a
Sebastian Palma Masabeu <sebastian.palma.masabeu@gmail.com>
2026-03-13 18:09:11 -0300

    feat : stacks

---
+++ data_structures/03_stacks/stacks.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class Stack:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.top = new_node
+        self.height = 1
+
+    def print_stack(self):
+        temp = self.top
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+    
+    def push(self,value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.height+=1
+        return True
+
+    def pop(self):
+        if self.height == 0: return False
+        if self.height ==1: self.top = None
+        top = self.top
+        self.top = top.next
+        top.next = None
+        self.height-=1
+        return top
+        
+
+stack = Stack(5)
+stack.print_stack()
+stack.push(1)
+stack.push(10)
+stack.print_stack()
+stack.pop()
+stack.print_stack()
